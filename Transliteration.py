82c8194b61e24b8459ec4f404bdd761e56b24712
sireesha rani <98334110+Sireeshatech@users.noreply.github.com>
2022-07-17 21:45:26 +0530

    Update Transliteration.py

--- Transliteration.py
+++ Transliteration.py
@@ -1,21 +1,44 @@
 #Language Transliteration
+#pip install indic-transliteration
 #pip install langdetect
-
+from indic_transliteration import sanscript
+from indic_transliteration.sanscript import transliterate
 from langdetect import detect
 
-#pip install indic-nlp-library
+text = input("Enter text in any INDIAN languages : ")
 
-from indicnlp.transliterate.unicode_transliterate import UnicodeIndicTransliterator
-from indicnlp.transliterate.unicode_transliterate import ItransTransliterator
-#write any language to the input_text from google
-#module is eligible for
-#Assamese(as),Bangali(be),Gujarati(gu),Hidi/urdu(hi,ur)
-#Marathi(ma),Nepali(ne),Odia(od),Punjab(pa),Sindhi(si),Sinhala(si),Sanskrit(sa)
-#Konkani(ko),Kannada(ka),Malyalam(ma),Telugu(te),Tamil(ta)
-input_text=('ఏమి చేస్తున్నారు')
+langdetect=detect(text)
 
-# Transliterate from Native language to any other Indian languages
-print(detect(input_text))
-print(UnicodeIndicTransliterator.transliterate(input_text,"te","hi"))
-#Transliterate from Native language to Roman script
-print(ItransTransliterator.to_itrans(input_text,'te'))
+#Languages={'DEVANAGARI':'hi','PUNJABI':'pa','GUJARATI':'gu', 'KANNADA':'kn',
+#'MALAYALAM':'ml','BENGALI':'bn','TELUGU':'te','TAMIL':'ta','ORIYA':'or'}
+
+# printing the transliterated text with the name of the input language
+if langdetect=='hi':
+    print("\nGiven Language is : HINDI\n")
+    print(transliterate(text,sanscript.DEVANAGARI,sanscript.ITRANS))
+elif langdetect=='pa':
+    print("\nGiven Language is : PUNJABI")
+    print(transliterate(text,sanscript.PUNJABI,sanscript.ITRANS))
+elif langdetect=='gu':
+    print("\nGiven Language is : GUJARATI")
+    print(transliterate(text,sanscript.GUJARATI,sanscript.ITRANS))
+elif langdetect=='kn':
+    print("\nGiven Language is : KANNADA")
+    print(transliterate(text,sanscript.KANNADA,sanscript.ITRANS))
+elif langdetect=='ml':
+    print("\nGiven Language is : MALAYALAM")
+    print(transliterate(text,sanscript.MALAYALAM,sanscript.ITRANS))
+elif langdetect=='bn':
+    print("\nGiven Language is : BENGALI")
+    print(transliterate(text,sanscript.BENGALI,sanscript.ITRANS))
+elif langdetect=='te':
+    print("\nGiven Language is : TELUGU")
+    print(transliterate(text,sanscript.TELUGU,sanscript.ITRANS))
+elif langdetect=='ta':
+    print("\nGiven Language is : TAMIL")
+    print(transliterate(text,sanscript.TAMIL,sanscript.ITRANS))
+elif langdetect=='or':
+    print("\nGiven Language is : ORIYA")
+    print(transliterate(text,sanscript.ORIYA,sanscript.ITRANS))
+else:
+    print("Can't able to transliterate this language")
