d5dcc37cee8321f8ccb8a4c94651e3fba931dc1b
sireesha rani <98334110+Sireeshatech@users.noreply.github.com>
2022-07-21 22:56:54 +0530

    Update Transliteration.py

--- Transliteration.py
+++ Transliteration.py
@@ -15,30 +15,30 @@ langdetect=detect(text)
 # printing the transliterated text with the name of the input language
 if langdetect=='hi':
     print("\nGiven Language is : HINDI\n")
-    print(transliterate(text,sanscript.DEVANAGARI,sanscript.VELTHUIS))
+    print(transliterate(text,sanscript.DEVANAGARI,sanscript.ITRANS))
 elif langdetect=='pa':
     print("\nGiven Language is : PUNJABI")
-    print(transliterate(text,sanscript.PUNJABI,sanscript.VELTHUIS))
+    print(transliterate(text,sanscript.PUNJABI,sanscript.ITRANS))
 elif langdetect=='gu':
     print("\nGiven Language is : GUJARATI")
-    print(transliterate(text,sanscript.GUJARATI,sanscript.VELTHUIS))
+    print(transliterate(text,sanscript.GUJARATI,sanscript.ITRANS))
 elif langdetect=='kn':
     print("\nGiven Language is : KANNADA")
-    print(transliterate(text,sanscript.KANNADA,sanscript.VELTHUIS))
+    print(transliterate(text,sanscript.KANNADA,sanscript.ITRANS))
 elif langdetect=='ml':
     print("\nGiven Language is : MALAYALAM")
-    print(transliterate(text,sanscript.MALAYALAM,sanscript.VELTHUIS))
+    print(transliterate(text,sanscript.MALAYALAM,sanscript.ITRANS))
 elif langdetect=='bn':
     print("\nGiven Language is : BENGALI")
-    print(transliterate(text,sanscript.BENGALI,sanscript.VELTHUIS))
+    print(transliterate(text,sanscript.BENGALI,sanscript.ITRANS))
 elif langdetect=='te':
     print("\nGiven Language is : TELUGU")
-    print(transliterate(text,sanscript.TELUGU,sanscript.VELTHUIS))
+    print(transliterate(text,sanscript.TELUGU,sanscript.ITRANS))
 elif langdetect=='ta':
     print("\nGiven Language is : TAMIL")
-    print(transliterate(text,sanscript.TAMIL,sanscript.VELTHUIS))
+    print(transliterate(text,sanscript.TAMIL,sanscript.ITRANS))
 elif langdetect=='or':
     print("\nGiven Language is : ORIYA")
-    print(transliterate(text,sanscript.ORIYA,sanscript.VELTHUIS))
+    print(transliterate(text,sanscript.ORIYA,sanscript.ITRANS))
 else:
     print("Can't able to transliterate this language")
